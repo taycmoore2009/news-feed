a12509211cf7993ab3e524a5361305f9476aa5a9
taycmoore2009 <tay.moore@chromeriver.com>
2019-06-23 13:47:04 -0700

    Inital layout

--- src/components/FeedComponent.js
+++ src/components/FeedComponent.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Container, Header } from 'semantic-ui-react'
-
 import './App.css';
 
-import newsFeed from './news.json';
-
 function App() {
-  console.log(newsFeed);
   return (
     <div className="App">
       <Container fluid>
-      <Header size='huge'>Your News Feed</Header>
-      
+      <Header size='medium'>Feed Item</Header>
       </Container>
     </div>
   );
